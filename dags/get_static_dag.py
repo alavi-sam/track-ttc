6f2499ced1b5b30dc6e3ba574a840f05df4d15d9
Sam Alavi <sam.alavi@hotmail.com>
2025-09-26 00:34:22 -0400

    Add S3 integration for fetching and uploading TTC static data; create new DAG for MinIO operations

--- dags/get_static_dag.py
+++ dags/get_static_dag.py
@@ -1,11 +1,13 @@
 from airflow import DAG
 from airflow.models import Variable
 from airflow.operators.python import PythonOperator, ShortCircuitOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 # from airflow.operators.bash import Bash
 import requests
 from zipfile import ZipFile
 from datetime import datetime, timedelta
 import os
+import io
 
 
 
@@ -24,7 +26,7 @@ default_args = {
 dag = DAG(
     dag_id='fetch_ttc_static_data',
     default_args=default_args,
-    schedule_interval='@daily',
+    schedule='@daily',
     catchup=False
 )
 
@@ -41,29 +43,53 @@ def check_for_static_update():
 
 
 def download_zip():
-    data_dir = '/tmp/data'
-    os.makedirs(data_dir, exist_ok=True)
-    response = requests.get(url, stream=True)
+    s3_hook = S3Hook(aws_conn_id='minio_s3_conn')
+
+    if not s3_hook.check_for_bucket('ttc-temp-data'):
+        s3_hook.create_bucket('ttc-temp-data')
+
+    response = requests.get(url)
     response.raise_for_status()
-    file_path = os.path.join(data_dir, 'static-ttc-file.zip')
-    with open(file_path, 'wb') as zf:
-        for chunk in response.iter_content(chunk_size=8192):
-            zf.write(chunk)
+
+    s3_hook.load_bytes(
+        bytes_data=response.content,
+        key='statics/static-ttc-file.zip',
+        bucket_name='ttc-temp-data',
+        replace=True
+    )
+    print("Uploaded zip file to MinIO")
     
     
 def extract_zip():
-    data_dir = '/tmp/data'
-    extract_dir = os.path.join(data_dir, 'static-ttc-files')
-    os.makedirs(extract_dir, exist_ok=True)
-    file_path = os.path.join(data_dir, 'static-ttc-file.zip')
-    with ZipFile(file_path, 'r') as zf:
-        zf.extractall(extract_dir)
-    print(os.listdir(extract_dir))
+    s3_hook = S3Hook(aws_conn_id='minio_s3_conn')
+    zip_file = s3_hook.get_key(
+        key='statics/static-ttc-file.zip',
+        bucket_name='ttc-temp-data',
+    )
+    zip_content = zip_file.get()['Body'].read()
+    with ZipFile(io.BytesIO(zip_content), 'r') as zf:
+        file_list = []
+        for file_info in zf.infolist():
+            if not file_info.is_dir():
+                file_content = zf.read(file_info.filename)
+                s3_hook.load_bytes(
+                    bytes_data=file_content,
+                    key=f'statics/{file_info.filename}',
+                    bucket_name='ttc-temp-data',
+                    replace=True
+                )
+                file_list.append(file_info.filename)
 
+        print('extracted files: ', file_list)
+
+        
 
 def clear_zip():
-    data_dir = '/tmp/data'
-    os.remove(os.path.join(data_dir, 'static-ttc-file.zip'))
+    s3_hook = S3Hook(aws_conn_id='minio_s3_conn')
+    s3_hook.delete_objects(
+        bucket='ttc-temp-data',
+        keys='statics/static-ttc-file.zip'
+    )
     print('deleted zip file!')
 
 
@@ -72,7 +98,8 @@ def update_etag():
     response.raise_for_status()
     file_version = response.headers.get('ETag')
     last_modified = response.headers.get('Last-Modified')
-    Variable.set('StaticFileLastModified', last_modified)
+    parsed_last_modified = datetime.strptime(last_modified, '%a, %d %b %Y %H:%M:%S GMT')
+    Variable.set('StaticFileLastModified', parsed_last_modified.strftime('%Y%m%d-%H%M%S'))
     Variable.set('StaticFileETag', file_version)
     print(f"Updated ETag to {file_version}")
 
---
+++ dags/insert_static_minio.py
@@ -0,0 +1,113 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.models import Variable
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.operators.python import PythonOperator, ShortCircuitOperator, BranchPythonOperator
+import os
+from urllib.parse import quote
+
+
+default_args = {
+    'owner': 'Sam',
+    'start_date': datetime(2025, 9, 18),
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'depends_on_past': 'False'
+}
+
+
+dag = DAG(
+    dag_id='static_files_to_minio',
+    default_args=default_args,
+    schedule_interval=None,
+    catchup=False
+)
+
+
+def check_bucket():
+    print("Starting bucket creation...")
+    s3_hook = S3Hook(aws_conn_id='minio_s3_conn')
+    if s3_hook.check_for_bucket('ttc-static-data'):
+        print('bucket already exists, skipping creation!')
+        return
+    print("S3Hook initialized, creating bucket...")
+    s3_hook.create_bucket('ttc-static-data')
+    print('bucket created!')
+
+
+def check_bucket_for_update():
+    last_modified = Variable.get('StaticFileLastModified')
+    etag = Variable.get('StaticFileETag')
+    etag_clean = quote(etag.strip('"'), safe='')
+    prefix = f"{etag_clean}-{last_modified}/"
+    bucket_name = 'ttc-static-data'
+
+
+    s3_hook = S3Hook(aws_conn_id='minio_s3_conn')
+    cond = s3_hook.check_for_prefix(
+        bucket_name=bucket_name,
+        prefix=prefix,
+        delimiter='/'
+    )
+
+    if cond:
+        print("prefix found. Static files not updated!")
+        return False
+    return True
+
+
+
+def upload_files():
+    s3_hook = S3Hook(aws_conn_id='minio_s3_conn')
+    last_modified = Variable.get('StaticFileLastModified')
+    etag = Variable.get('StaticFileETag')
+    etag_clean = quote(etag.strip('"'), safe='')
+    bucket_name = 'ttc-static-data'
+    temp_bucket = 'ttc-temp-data'
+    prefix = f"{etag_clean}-{last_modified}/"
+
+    file_keys = s3_hook.list_keys(
+        bucket_name=temp_bucket,
+        prefix='statics/',
+        delimiter='/'
+    )
+
+    for file in file_keys:
+        filename = file.split('/')[-1]  
+        s3_hook.copy_object(
+            source_bucket_key=file,
+            source_bucket_name=temp_bucket,
+            dest_bucket_key=f"{prefix}{filename}", 
+            dest_bucket_name=bucket_name
+        )
+    s3_hook.delete_objects(
+        bucket=temp_bucket,
+        keys=file_keys
+    )
+
+
+    print(f"Uploaded {len(file_keys)} files to bucket {bucket_name} with prefix {prefix}")
+
+
+check_bucket_operator = PythonOperator(
+    dag=dag,
+    task_id='create_bucket',
+    python_callable=check_bucket
+)
+
+
+check_bucket_for_update_operator = ShortCircuitOperator(
+    dag=dag,
+    task_id='check_bucket_for_update',
+    python_callable=check_bucket_for_update
+)
+
+
+upload_files_operator = PythonOperator(
+    dag=dag,
+    task_id='upload_files',
+    python_callable=upload_files,
+)
+
+
+check_bucket_operator >> check_bucket_for_update_operator >> upload_files_operator
